e8cf5926527e6c4b071c3443317442d4fccb426e
Ryan Nguyen <ryanwin@Ryans-MacBook-Pro.local>
2026-04-28 17:14:39 -0400

    Added demo file, test file, txt file, and created hash map

--- Milestone2.py
+++ Milestone2.py
@@ -75,3 +75,102 @@ class LinkedQueue:
     
     def __len__(self):
         return self._size
+
+# Class for key value pairs
+class Entry:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __str__(self):
+        return str(self.key) + ":" + str(self.value)
+
+# Stores key-value pairs in a list
+class ListMapping:
+    def __init__(self):
+        self._entries = []
+    
+    def _entry(self, key):
+        for e in self._entries:
+            if e.key == key:
+                return e
+        return None
+    
+    def put(self, key, value):
+        e = self._entry(key)
+        if e is not None:
+            e.value = value
+        else:
+            self._entries.append(Entry(key, value))
+
+    def get(self, key):
+        e = self._entry(key)
+        if e is not None:
+            return e.value
+        else:
+            return ValueError
+    
+    def __len__(self):
+        return len(self._entries)
+    
+    def __str__(self):
+        return str([str(e) for e in self._entries])
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+    
+    def __getitem__(self, key):
+        return self.get(key)
+    
+    def __contains__(self, key):
+        e = self._entry(key)
+        if e is not None:
+            return True
+        else:
+            return False
+    
+    def items(self):
+        return ((e.key, e.value) for e in self._entries)
+    
+# Hash Map
+class HashMapping:
+    def __init__(self, size = 2):
+        self._size = size
+        self._buckets = [ListMapping() for i in range(self._size)]
+        self._length = 0
+    
+    def _bucket(self, key):
+        return self._buckets[hash(key) % self._size]
+    
+    def rehashing(self):
+        print("I need more space")
+        old_buckets = self._buckets
+        self._size *= 2
+        self._buckets = [ListMapping() for i in range(self._size)]
+
+        for bucket in old_buckets:
+            for key, value in bucket.items():
+                m = self._bucket(key)
+                m[key] = value
+        
+    def __len__(self):
+        return self._length
+    
+    def __setitem__(self, key, value):
+        m = self._bucket(key)
+        if key not in m:
+            self._length += 1
+        m[key] = value
+
+        if self._length / self._size >= 0.8:
+            self.rehashing()
+    
+    def __getitem__(self, key):
+        m = self._bucket(key)
+        return m[key]
+
+    def __repr__(self):
+        output = "Hash Table Structure:\n"
+        for i in range(len(self._buckets)):
+            output += f"Bucket {i}: {self._buckets[i]}\n"
+        return output
